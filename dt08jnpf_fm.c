ada28e0fee2e77c1e8e7944abe8e8244862f50af
Patrick Forsyth <patrick.forsyth@gmail.com>
2013-08-14 09:49:50 +0200

    indenting fixed

--- dt08jnpf_fm.c
+++ dt08jnpf_fm.c
@@ -71,70 +71,70 @@ static int fm_elim(size_t rows, size_t cols, rational_t** a, rational_t* c)
        t = t_sort;
        
        // - Can be done with one for-loop?
-       for(i = 0; i < r; i += 1)
-           for(j = 0; j < n2; j += 1)
+       	for(i = 0; i < r; i += 1)
+           	for(j = 0; j < n2; j += 1)
                rat_div(&t[j][i], &t[j][r]);
-       for(j = 0; j < n2; j += 1)
+       	for(j = 0; j < n2; j += 1)
            rat_div(&q[j], &t[j][r]);
-       if(r == 0){
-        rational_t tmp;
-	tmp.num = 100000000;
-	tmp.den = 1;
+       	if(r == 0){
+       	rational_t tmp;
+		tmp.num = 100000000;
+		tmp.den = 1;
         int min = 0;
 
-	int max = 0;
-        for(i = 0; i < n1; i +=1)
-            if(rat_cmp(&q[i], &tmp) < 0){
-                tmp.num = q[i].num;
-		tmp.den = q[i].den;
-                min = i;
-            }
+		int max = 0;
+        	for(i = 0; i < n1; i +=1)
+            	if(rat_cmp(&q[i], &tmp) < 0){
+              	  	tmp.num = q[i].num;
+					tmp.den = q[i].den;
+              	 	min = i;
+            	}
 
-	tmp.num = 0;
-	tmp.den = 1;
+		tmp.num = 0;
+		tmp.den = 1;
 
-	for(i = n1; i < n2; i +=1)
-            if(rat_cmp(&q[i], &tmp) > 0 ){
-                tmp.num = q[i].num;
-		tmp.den = q[i].den;
-                max = i;
-            }
+		for(i = n1; i < n2; i +=1)
+	        if(rat_cmp(&q[i], &tmp) > 0 ){
+	            tmp.num = q[i].num;
+				tmp.den = q[i].den;
+	            max = i;
+	        }
        
-          if (rat_cmp(&q[max], &q[min]) > 0) //b1 > B1
-            return false;
-        for(; n2 < s; n2 += 1)
-            if(rat_cmp(&q[n2], &ZERO) < 0)
-                return false;
-        return true;
-	}
-	size_t s_pr = s - n2 + n1 * (n2 - n1);
-	if (s_pr == 0)
-		return true;
-	
-	rational_t **t2 = malloc((r-1)*sizeof(rational_t*));
-    rational_t *q2 = malloc(s_pr*sizeof(rational_t));
-	for (i = 0; i < r-1; i += 1)
-        t2[i] = malloc(s_pr*sizeof(rational_t));
-    int k;
-    for(i = 0; i < n1; i += 1)
-        for(j = n1; j < n2; j += 1){
-			rat_sub(&q[i],&q[j]);
-            q2[i*(n2-n1)+j-n1] = q[i];
-    		for(k = 0; k < r-1; k += 1) {
-				rat_sub(&t[i][k],&t[j][k]);
-                t2[i*(n2-n1)+j-n1][k] = t[i][k];  
-			}     
+	        if (rat_cmp(&q[max], &q[min]) > 0) //b1 > B1
+	            return false;
+	        for(; n2 < s; n2 += 1)
+	            if(rat_cmp(&q[n2], &ZERO) < 0)
+	                return false;
+	        return true;
 		}
-	for(i = n2; i < s; i += 1){
-   		q2[s_pr-s+i] = q[i];
-    	for(k = 0; k < r-1; k += 1)
-        	t2[s_pr-s+i][k] = t[i][k];       
-		}
-	free(t);
-	free(q);
-	q = q2;
-	t = t2;
-   }    
+		size_t s_pr = s - n2 + n1 * (n2 - n1);
+		if (s_pr == 0)
+			return true;
+		
+		rational_t **t2 = malloc((r-1)*sizeof(rational_t*));
+	    rational_t *q2 = malloc(s_pr*sizeof(rational_t));
+		for (i = 0; i < r-1; i += 1)
+	        t2[i] = malloc(s_pr*sizeof(rational_t));
+	    int k;
+	    for(i = 0; i < n1; i += 1)
+	        for(j = n1; j < n2; j += 1){
+				rat_sub(&q[i],&q[j]);
+	            q2[i*(n2-n1)+j-n1] = q[i];
+	    		for(k = 0; k < r-1; k += 1) {
+					rat_sub(&t[i][k],&t[j][k]);
+	                t2[i*(n2-n1)+j-n1][k] = t[i][k];  
+				}     
+			}
+		for(i = n2; i < s; i += 1){
+	   		q2[s_pr-s+i] = q[i];
+	    	for(k = 0; k < r-1; k += 1)
+	        	t2[s_pr-s+i][k] = t[i][k];       
+			}
+		free(t);
+		free(q);
+		q = q2;
+		t = t2;
+	}    
 }
 
 void print_all(rational_t** A, rational_t* cc, size_t r, size_t c) {
